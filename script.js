100ee6e696cfb8be5256bb3f340f4fb6111528ce
Pawan Kumar <65233567+itzpa1@users.noreply.github.com>
2024-07-15 16:55:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,10 +24,12 @@ muteAudio.addEventListener('click', function () {
 
 var birthdayArea = document.querySelector(".birthdayCard_Area");
 var birthdayShow = document.querySelector(".tap");
+var birthdayAudio = document.querySelector(".hbd_audio");
 
 birthdayShow.addEventListener('click', function () {
     birthdayArea.style.display = "flex";
     birthdayArea.classList.add("open");
+    birthdayAudio.play();
 })
 
 // hide birthdayPop up 
@@ -35,25 +37,25 @@ birthdayShow.addEventListener('click', function () {
 var hbd_audio = document.querySelector(".hbd_audio");
 var infoPopup = document.querySelector(".info_popup");
 var popClose = document.getElementById("pop_close");
+const cardsContainer = document.querySelector(".cards");
 
 hbd_audio.addEventListener('ended', function () {
+    birthdayShow.style.display = "none";
     birthdayArea.classList.remove("open");
     birthdayArea.classList.add("close");
     infoPopup.style.display = "flex";
     infoPopup.classList.add("open")
+    cardsContainer.style.display = "flex"
 })
 
 popClose.addEventListener('click', function () {
     infoPopup.style.display = "none";
 })
 
-var musicCount = document.querySelector(".music_count");
-
 function playAudio(obj) {
     var cardId = "#" + obj.id + " audio";
     var cardAudio = document.querySelector(cardId);
     cardAudio.play()
-    // musicCount = cardAudio.curre
 }
 
 
@@ -61,7 +63,7 @@ function playAudio(obj) {
 // adding card dynamically
 
 
-var arrayTest = [
+var cardData = [
     {
         musicName: "This Gurll",
         imgSrc: `<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Face%20Holding%20Back%20Tears.png" alt="Face Holding Back Tears"/>`,
@@ -124,72 +126,40 @@ var arrayTest = [
     }
 ]
 
-// window.onload = function () {
-//     arrayTest.forEach(function (item, index) {
-//         $(".cards").eq(index).append(
-//             `<div id="${item.bgSrc}" class="card relative w-[120px] h-[150px] rounded-lg shrink-0" onclick="playAudio(this)">
-//                 <div class="card-inner">
-//                     <div class="card-front relative flex justify-center items-end bg-white" >
-//                         <div class="center_box relative w-[70%] h-[55%] mb-6 rounded-md flex justify-center items-center z-10">
-//                             ${item.imgSrc}
-//                         </div>
-//                     </div>
-//                     <div class="card-back relative flex justify-center items-end" >
-//                         <img class="bg_img h-full w-full absolute object-cover" src="media/img/${item.bgSrc}.jpg" alt="">
-//                         <div class="center_box relative w-[80%] h-auto mb-2 rounded-md flex flex-col justify-center items-end z-10">
-//                             <!-- <h1 class="music_count text-2xl font-semibold mr-1">00</h1> -->
-//                             <h1 class="music_name text-sm font-semibold w-full text-left pl-1">${item.musicName}</h1>
-//                             <audio class="hidden" src="media/audio/${item.musicName}.mp3" type="audio/mp3"></audio>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>`
-//         )
-//     })
-// }
 
-// const Content = `<div id="${card_ID}" class="card relative w-[120px] h-[150px] rounded-lg shrink-0" onclick="playAudio(this)">
-//                 <div class="card-inner">
-//                     <div class="card-front relative flex justify-center items-end bg-white" >
-//                         <div class="center_box relative w-[70%] h-[55%] mb-6 rounded-md flex justify-center items-center z-10">
-//                             ${img_src}
-//                         </div>
-//                     </div>
-//                     <div class="card-back relative flex justify-center items-end" >
-//                         <img class="h-full w-full absolute object-cover" src="media/img/${bg_src}.png" alt="">
-//                         <div class="center_box relative w-[80%] h-auto mb-2 rounded-md flex flex-col justify-center items-end z-10">
-//                             <!-- <h1 class="music_count text-2xl font-semibold mr-1">00</h1> -->
-//                             <h1 class="music_name text-sm font-semibold w-full text-left pl-1">${music_name}</h1>
-//                             <audio class="hidden" src="media/audio/${music_src}.mp3" type="audio/mp3"></audio>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>`;
-
-window.onload = function(){
-    // cards.appendChild
-    for (let i = 0; i < 12; i++) {
-        arrayTest.forEach(function (item, index) {
-            console.log(index)
-            $(".cards").eq(index).append(
-                `<div id="${item.bgSrc}" class="card relative w-[120px] h-[150px] rounded-lg shrink-0" onclick="playAudio(this)">
-                    <div class="card-inner">
-                        <div class="card-front relative flex justify-center items-end bg-white" >
-                            <div class="center_box relative w-[70%] h-[55%] mb-6 rounded-md flex justify-center items-center z-10">
-                                ${item.imgSrc}
-                            </div>
-                        </div>
-                        <div class="card-back relative flex justify-center items-end" >
-                            <img class="bg_img h-full w-full absolute object-cover" src="media/img/${item.bgSrc}.jpg" alt="">
-                            <div class="center_box relative w-[80%] h-auto mb-2 rounded-md flex flex-col justify-center items-end z-10">
-                                <!-- <h1 class="music_count text-2xl font-semibold mr-1">00</h1> -->
-                                <h1 class="music_name text-sm font-semibold w-full text-left pl-1">${item.musicName}</h1>
-                                <audio class="hidden" src="media/audio/${item.musicName}.mp3" type="audio/mp3"></audio>
-                            </div>
+const cardsMethods = () => {
+    cardData.map((item) => {
+        const Content = `<div id="${item.bgSrc}" class="card relative w-[120px] h-[150px] rounded-lg shrink-0" onclick="playAudio(this)">
+                <div class="card-inner">
+                    <div class="card-front relative flex justify-center items-end bg-white" >
+                        <div class="center_box relative w-[70%] h-[55%] mb-6 rounded-md flex justify-center items-center z-10">
+                            ${item.imgSrc}
                         </div>
                     </div>
-                </div>`
-            )
-        })
-    }
+                    <div class="card-back relative flex justify-center items-end" >
+                        <img class="h-full w-full absolute object-cover" src="media/img/${item.bgSrc}.jpg" alt="">
+                        <div class="center_box relative w-[80%] h-auto mb-2 rounded-md flex flex-col justify-center items-end z-10">
+                            <!-- <h1 class="music_count text-2xl font-semibold mr-1">00</h1> -->
+                            <h1 class="music_name text-sm font-semibold w-full text-left pl-1">${item.musicName}</h1>
+                            <audio class="hidden" src="media/audio/${item.musicName}.mp3" type="audio/mp3"></audio>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+        cardsContainer.innerHTML += Content;
+        // console.log(Content)
+    })
 }
+
+cardsMethods()
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for (var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
+
